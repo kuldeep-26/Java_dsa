49fbf716c9fae36a0b6511495302c06a45b57abc
Harsh Singh <harsh2662004@gmail.com>
2025-08-04 21:23:56 +0530

    Sorting using basic approach

---
+++ Sorting/solution.java
@@ -0,0 +1,85 @@
+public class solution{
+
+    public static void printArray(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void bubbleSort(int arr[]){
+        for(int i = 0; i < arr.length-1; i++){
+            for(int j = 0; j < arr.length-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void selectionSort(int arr[]){
+        for(int i = 0; i < arr.length-1; i++){
+            int minPos = i;
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[minPos] > arr[j]){
+                    minPos = j;
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void insertionSort(int arr[]){
+        for(int i = 1; i < arr.length; i++){
+            int curr = arr[i];
+            int prev = i - 1;
+            // Finding correct position....
+            while(prev >= 0 && arr[prev] > curr){
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            // inserting into position.
+            arr[prev + 1] = curr;
+        }
+    }
+
+    public static void countingSort(int arr[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            largest = Math.max(largest,arr[i]);
+        }
+        // creating frequency array....
+        int count[] = new int[largest + 1];
+        for(int i = 0; i < arr.length; i++){
+            count[arr[i]]++;
+        }
+        // sorting....
+        int j = 0;
+        for(int i = 0; i < count.length; i++){
+            while(count[i] > 0){
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        int arr[] = {5,4,1,3,2};
+        // bubbleSort(arr);
+        // printArray(arr);
+
+        // selectionSort(arr);
+        // printArray(arr);
+
+        // insertionSort(arr);
+        // printArray(arr);
+
+        countingSort(arr);
+        printArray(arr);
+    }
+}
