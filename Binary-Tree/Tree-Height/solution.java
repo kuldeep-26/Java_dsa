b91fffe6fa8e48de10466fc95b266fe1fc62a329
Harsh Singh <harsh2662004@gmail.com>
2025-08-31 15:57:04 +0530

    Sum and count of nodes

--- Binary-Tree/Tree-Height/solution.java
+++ Binary-Tree/Tree-Height/solution.java
@@ -22,6 +22,26 @@ public class solution{
         return maxHeight;
     }
 
+    public static int count(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftCount = count(root.left);
+        int rightCount = count(root.right);
+
+        return leftCount + rightCount + 1;
+    }
+
+    public static int sum(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftSum = sum(root.left);
+        int rightSum = sum(root.right);
+
+        return leftSum + rightSum + root.data;
+    }
+
     public static void main(String[] args){
         Node root = new Node(1);
         root.left = new Node(2);
@@ -31,6 +51,10 @@ public class solution{
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        System.out.println(height(root));
+        System.out.println("Height of Tree: " + height(root));
+
+        System.out.println("Total no. of nodes in Tree: " + count(root));
+
+        System.out.println("Sum of nodes: " + sum(root));
     }
 }
