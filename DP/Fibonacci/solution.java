c37e78268defd2a867353143e9696d7c7323a7cd
Harsh Singh <harsh2662004@gmail.com>
2025-09-26 21:03:46 +0530

    Refactor Fibonacci solution to improve clarity and update main method for testing using tabulation method

--- DP/Fibonacci/solution.java
+++ DP/Fibonacci/solution.java
@@ -11,9 +11,20 @@ public class solution{
         return f[n];
     }
 
+    public static int fib_tab(int n){
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];   // ans...
+    }
+
     public static void main(String[] args){
         int n = 5;
         int f[] = new int[n+1];   // {0, 0, 0,...}
-        System.out.println(fib(n, f));
+        // System.out.println(fib(n, f));
+        System.out.println(fib_tab(6));
     }
 }
