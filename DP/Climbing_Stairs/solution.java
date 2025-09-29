15e551b87be9cfe46877acff8bebfcf728eefca1
Harsh Singh <harsh2662004@gmail.com>
2025-09-29 16:58:38 +0530

    Enhance Climbing Stairs solution with memoization and improve output formatting

--- DP/Climbing_Stairs/solution.java
+++ DP/Climbing_Stairs/solution.java
@@ -1,4 +1,7 @@
+import java.util.*;
+
 public class solution{
+
     public static int climbStairs(int n){
         if(n == 0){
             return 1;
@@ -6,12 +9,28 @@ public class solution{
         if(n < 0){
             return 0;
         }
-        int ways = climbStairs(n-1) + climbStairs(n-2);
-        return ways;
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+
+    public static int climbStairsMemo(int ways[], int n){
+        if(n == 0){
+            return 1;
+        }
+        if(n < 1){
+            return 0;
+        }
+        if(ways[n] != -1){
+            return ways[n];
+        }
+        ways[n] = climbStairsMemo(ways, n-1) + climbStairsMemo(ways, n-2);
+        return ways[n];
     }
 
     public static void main(String[] args){
         int n = 5;
-        System.out.println(climbStairs(n));
+        int ways[] = new int[n+1];
+        Arrays.fill(ways, -1);
+        System.out.println("Counting Ways Using Simple Recursion: " + climbStairs(n));
+        System.out.println("Counting Ways Using Memoization: " + climbStairsMemo(ways, n));
     }
 }
