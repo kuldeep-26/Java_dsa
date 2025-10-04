37aae43339506937f5d5a4a8214abf59656018af
Harsh Singh <harsh2662004@gmail.com>
2025-10-04 19:44:46 +0530

    Implement memoization for 0-1 Knapsack solution to optimize performance

--- DP/Knapsack_0-1/solution.java
+++ DP/Knapsack_0-1/solution.java
@@ -14,10 +14,37 @@ public class solution{
         }
     }
 
+    public static int knapSackMemo(int val[], int wt[], int W, int i, int dp[][]){
+        if(W == 0 || i == 0){
+            return 0;
+        }
+
+        if(dp[i][W] != -1){
+            return dp[i][W];
+        }
+
+        if(wt[i-1] <= W){
+            int ans1 = val[i-1] + knapSackMemo(val, wt, W-wt[i-1], i-1, dp);
+            int ans2 = knapSackMemo(val, wt, W, i-1, dp);
+            dp[i][W] = Math.max(ans1, ans2);
+            return dp[i][W];
+        }else{
+            dp[i][W] = knapSackMemo(val, wt, W, i-1, dp);
+            return dp[i][W];
+        }
+    }
+
     public static void main(String[] args){
         int val[] = {15, 14, 10, 45, 30};
         int wt[] = {2, 5, 1, 3, 4};
         int W = 7;
-        System.out.println(knapSack(val, wt, W, val.length));
+        int dp[][] = new int[val.length + 1][W + 1];
+        for(int i=0; i<dp.length; i++){
+            for(int j=0; j<dp[0].length; j++){
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println("Max Profit of knapsack: " + knapSack(val, wt, W, val.length));
+        System.out.println("Max Profit by Memo.: " + knapSackMemo(val, wt, W, val.length, dp));
     }
 }
