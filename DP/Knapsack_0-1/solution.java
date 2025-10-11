2acc01f2c00500a98da8143fcfc8f2ffb613dc34
Harsh Singh <harsh2662004@gmail.com>
2025-10-11 13:28:53 +0530

    Add tabulation method for 0-1 Knapsack problem and print function

--- DP/Knapsack_0-1/solution.java
+++ DP/Knapsack_0-1/solution.java
@@ -34,6 +34,42 @@ public class solution{
         }
     }
 
+    public static void print(int dp[][]){
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < dp[0].length; j++){
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int knapSackTab(int val[], int wt[], int W){
+        int n = val.length;
+        int dp[][] = new int[n+1][W+1];
+        for(int i = 0; i < dp.length; i++){
+            dp[i][0] = 0;
+        }
+        for(int j = 0; j < dp[0].length; j++){
+            dp[0][j] = 0;
+        }
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < W+1; j++){
+                int v = val[i-1];   // ith item value
+                int w = wt[i-1];    // ith item weight
+                if(w <= j){    // valid
+                    int incProfit = v + dp[i-1][j-w];
+                    int excProfit = dp[i-1][j];
+                    dp[i][j] = Math.max(incProfit, excProfit);
+                }else{     // invalid
+                    int excProfit = dp[i-1][j];
+                    dp[i][j] = excProfit;
+                }
+            }
+        }
+        print(dp);
+        return dp[n][W];
+    }
+
     public static void main(String[] args){
         int val[] = {15, 14, 10, 45, 30};
         int wt[] = {2, 5, 1, 3, 4};
@@ -44,7 +80,9 @@ public class solution{
                 dp[i][j] = -1;
             }
         }
+
         System.out.println("Max Profit of knapsack: " + knapSack(val, wt, W, val.length));
         System.out.println("Max Profit by Memo.: " + knapSackMemo(val, wt, W, val.length, dp));
+        System.out.println("Max Profit by Tabulation: " + knapSackTab(val, wt, W));
     }
 }
